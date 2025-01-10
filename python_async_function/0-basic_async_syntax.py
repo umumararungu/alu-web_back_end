0203b334029e2799eb63d3078b96455cea35bda8
umumararungu <umcynth@gmail.com>
2025-01-10 22:16:26 +0200

    alll done

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 '''0. The basics of async'''
 
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 '''1. Let's execute multiple coroutines at the same time with async'''
 import asyncio
