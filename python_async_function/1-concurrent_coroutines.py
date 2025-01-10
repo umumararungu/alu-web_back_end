6d8c5f7c4029915beb8b8665ff50d708f6afbb46
umumararungu <umcynth@gmail.com>
2025-01-10 22:12:51 +0200

    done

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,6 @@
 
 #!/usr/bin/env python3
 '''1. Let's execute multiple coroutines at the same time with async'''
-
-
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,6 +6,7 @@
 import time
 import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
+'module'
 
 
 def measure_time(n: int, max_delay: int) -> float:
