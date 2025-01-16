c44e7bc205a60ded703ab2524af1ec28200376a4
umumararungu <umcynth@gmail.com>
2025-01-16 15:01:02 +0200

    created tuple

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int,int]:
+    "helper function"
+    return ((page - 1) * page_size, page * page_size)
