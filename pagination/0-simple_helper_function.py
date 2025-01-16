826a8c479d9f5bfa5a37b42236ecd91a54659040
umumararungu <umcynth@gmail.com>
2025-01-16 15:01:59 +0200

    spaces

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """Simple helper function"""
 from typing import Tuple
 
-def index_range(page: int, page_size: int) -> Tuple[int,int]:
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     "helper function"
     return ((page - 1) * page_size, page * page_size)
