7c9655517a15da2b22314bdbfe7e395971d12444
umumararungu <umcynth@gmail.com>
2025-01-16 22:48:49 +0200

    space

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -26,16 +26,18 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert type(page) == int
-            assert type(page_size) == int
-            assert page > 0
-            assert page_size > 0
-            csv_size = len(self.dataset())
-            start, end = index_range(page, page_size)
-            end = min(end, csv_size)
-            if start >= csv_size:
-                return []
-            return self.dataset()[start:end]
+        """ Get page function
+        """
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page > 0
+        assert page_size > 0
+        csv_size = len(self.dataset())
+        start, end = index_range(page, page_size)
+        end = min(end, csv_size)
+        if start >= csv_size:
+            return []
+        return self.dataset()[start:end]
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
